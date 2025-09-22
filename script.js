08d719d281c97b653265416281b0240e2362cf7d
finvest2005 <finvest2005@yandex.ru>
2025-09-22 21:40:39 +0300

    Add hoovering effect over grid cells

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ for (let i = 1; i <= 16; i++) {
   elem.classList.add('row' + i);
   for (let j = 1; j <= 16; j++) {
     const elem_row = document.createElement('div');
-    elem_row.classList.add('col' + j);
+    elem_row.classList.add('row' + i + 'col' + j);
     elem_row.style.border = '1px solid black';
     elem_row.style.width = (100 / 16).toFixed(2) + 'vw';
     elem.appendChild(elem_row);
@@ -18,3 +18,13 @@ for (let i = 1; i <= 16; i++) {
   content.push(elem);
   container.appendChild(elem);
 }
+
+container.addEventListener('mouseover', function (e) {
+  const elem = e.target;
+  elem.style.backgroundColor = 'pink';
+});
+
+container.addEventListener('mouseout', function (e) {
+  const elem = e.target;
+  elem.style.backgroundColor = 'white';
+});
