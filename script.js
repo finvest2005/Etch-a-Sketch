9ec656f4b9dc4f8e8dc3afcb5758a02b91f4d989
finvest2005 <finvest2005@yandex.ru>
2025-09-22 22:13:56 +0300

    Create possibility of changing grid size on button click
    Grid redraw itself accordingly

--- script.js
+++ script.js
@@ -1,30 +1,50 @@
+let numberCells = 16;
 const container = document.querySelector('.container');
 
-const content = [];
-for (let i = 1; i <= 16; i++) {
-  const elem = document.createElement('div');
-  elem.style.border = '1px solid black';
-  elem.style.height = (100 / 16).toFixed(2) + 'vh';
-  elem.style.display = 'flex';
-  elem.style.flexDirection = 'row';
-  elem.classList.add('row' + i);
-  for (let j = 1; j <= 16; j++) {
-    const elem_row = document.createElement('div');
-    elem_row.classList.add('row' + i + 'col' + j);
-    elem_row.style.border = '1px solid black';
-    elem_row.style.width = (100 / 16).toFixed(2) + 'vw';
-    elem.appendChild(elem_row);
+function changeGrid() {
+  let xx = prompt('Enter new grid Size (from 2 to 100)', '16');
+  let num = parseInt(xx);
+  while (xx < 2 || xx > 100) {
+    alert('Keep size of grid from 2 to 100');
+    xx = prompt('Enter new grid Size (from 2 to 100)', '16');
+    num = parseInt(xx);
   }
-  content.push(elem);
-  container.appendChild(elem);
+  container.replaceChildren();
+  console.log(num);
+  numberCells = num;
+  drawGrid();
 }
 
-container.addEventListener('mouseover', function (e) {
-  const elem = e.target;
-  elem.style.backgroundColor = 'pink';
-});
+function drawGrid() {
+  for (let i = 1; i <= numberCells; i++) {
+    const elem = document.createElement('div');
+    elem.style.border = '1px solid black';
+    elem.style.height = (90 / numberCells - 0.01).toFixed(2) + 'vh';
+    elem.style.display = 'flex';
+    elem.style.flexDirection = 'row';
+    elem.classList.add('row' + i);
+    for (let j = 1; j <= numberCells; j++) {
+      const elem_row = document.createElement('div');
+      elem_row.classList.add('row' + i + 'col' + j);
+      elem_row.style.border = '1px solid black';
+      elem_row.style.width = (90 / numberCells - 0.01).toFixed(2) + 'vw';
+      elem.appendChild(elem_row);
+    }
+    container.appendChild(elem);
+  }
 
-container.addEventListener('mouseout', function (e) {
-  const elem = e.target;
-  elem.style.backgroundColor = 'white';
-});
+  container.addEventListener('mouseover', function (e) {
+    const elem = e.target;
+    elem.style.backgroundColor = 'pink';
+  });
+
+  container.addEventListener('mouseout', function (e) {
+    const elem = e.target;
+    elem.style.backgroundColor = 'white';
+  });
+
+  const button = document.querySelector('.changeGrid');
+  button.addEventListener('click', changeGrid);
+}
+
+drawGrid();
